f2b1971dc6f8fd9c43f9bca9ac7132bf98a2b63e
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-05-12 01:29:13 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function validateForm() {
+    var age = document.getElementById('age').value;
+    var name = document.getElementById('name').value;
+
+    if (age === "" || name === "") {
+        alert("Inputs cannot be empty!");
+        return false;
+    }
+
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if (age > 18) {
+                alert("Welcome, " + name + ". You can vote.");
+                resolve();
+            } else {
+                alert("Oh sorry " + name + ". You aren't old enough.");
+                reject();
+            }
+        }, 4000); // 4 seconds timeout
+    });
+}
