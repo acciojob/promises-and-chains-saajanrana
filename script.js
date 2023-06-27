a50423597f7228a4bc4f2eef25ad5f7f5b8dd908
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 02:27:31 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-//your JS code here. If required.
-function validateForm() {
-    var age = document.getElementById('age').value;
-    var name = document.getElementById('name').value;
-
-    if (age === "" || name === "") {
-        alert("Inputs cannot be empty!");
-        return false;
-    }
-
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            if (age > 18) {
-                alert("Welcome, " + name + ". You can vote.");
-                resolve();
-            } else {
-                alert("Oh sorry " + name + ". You aren't old enough.");
-                reject();
-            }
-        }, 4000); // 4 seconds timeout
+function submitForm(event) {
+  event.preventDefault(); // Prevent form submission
+  
+  const ageInput = document.getElementById('age');
+  const nameInput = document.getElementById('name');
+  
+  const age = parseInt(ageInput.value);
+  const name = nameInput.value;
+  
+  if (age && name) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(name);
+        } else {
+          reject(name);
+        }
+      }, 4000);
     });
+    
+    promise
+      .then((name) => {
+        alert(`Welcome, ${name}. You can vote.`);
+      })
+      .catch((name) => {
+        alert(`Oh sorry ${name}. You aren't old enough.`);
+      });
+      
+    ageInput.value = '';
+    nameInput.value = '';
+  }
 }
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', submitForm);
